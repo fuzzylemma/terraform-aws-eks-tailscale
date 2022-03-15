c3370f4481a599dc5a0fae26c2b32591ffd3a754
alfred <fuzzy@lemmasis.org>
2022-03-15 02:11:36 -0400

    wip : skeleton tailscale modules

---
+++ tailscale-service-proxy/variables.tf
@@ -0,0 +1,12 @@
+variable "name" {
+}
+variable "app" {
+}
+variable "replicas" {}
+variable "service_account_name" {}
+variable "state_secret_name" {}
+variable "userspace" {}
+variable "tailscale_auth_key" {}
+variable "service_cluster_ip" {}
+variable "destination_ip" {}
+
