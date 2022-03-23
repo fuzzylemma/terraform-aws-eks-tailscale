8e38b1bc399849a16a570bc8c9e8db9af3434a3c
alfred <fuzzy@lemmasis.org>
2022-03-23 18:42:13 -0400

    Add versions

---
+++ versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.6.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.9.0"
+    }
+  }
+
+  required_version = ">= 1.1.7"
+}
+
